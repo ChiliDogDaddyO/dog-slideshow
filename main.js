04c38e25789d71f6176f6b8ca3cffc961ce0d130
hpz2 <sallen1026@gmail.com>
2020-09-07 07:48:39 -0400

    at "1:01:55 edge cases" mark

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let timer
+let deleteFirstPhotoDelay
+
 async function start() {
   const response = await fetch('https://dog.ceo/api/breeds/list/all')
   const data = await response.json()
@@ -24,5 +27,32 @@ async function loadByBreed(breed) {
     const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
     const data = await response.json()
     console.log(data)
+    createSlideshow(data.message)
+  }
+}
+
+function createSlideshow(images) {
+  let currentPosition = 0
+  clearInterval(timer)
+  clearTimeout(deleteFirstPhotoDelay)
+  document.getElementById('slideshow').innerHTML = `
+  <div class="slide" style="background-image: url('${images[0]}');"></div>
+  <div class="slide" style="background-image: url('${images[1]}');"></div>
+  `
+  currentPosition += 2
+  timer = setInterval(nextSlide, 3000)
+
+  function nextSlide() {
+    document.getElementById('slideshow').insertAdjacentHTML(
+      'beforeend',`<div class="slide" style="background-image: url('${images[currentPosition]}');"></div>`
+    )
+    deleteFirstPhotoDelay = setTimeout(function () {
+      document.querySelector(".slide").remove()
+    } ,1000)
+    if (currentPosition + 1 >= images.length) {
+      currentPosition = 0
+    } else {
+      currentPosition++
+    }
   }
 }
