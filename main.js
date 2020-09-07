2c4f8c8e6c499ee923562c26a08e701cccf0701d
hpz2 <sallen1026@gmail.com>
2020-09-07 07:57:48 -0400

    at "1:05:05 Try/Catch" mark

--- main.js
+++ main.js
@@ -35,12 +35,21 @@ function createSlideshow(images) {
   let currentPosition = 0
   clearInterval(timer)
   clearTimeout(deleteFirstPhotoDelay)
-  document.getElementById('slideshow').innerHTML = `
+
+  if (images.length > 1) {
+      document.getElementById('slideshow').innerHTML = `
   <div class="slide" style="background-image: url('${images[0]}');"></div>
   <div class="slide" style="background-image: url('${images[1]}');"></div>
   `
-  currentPosition += 2
-  timer = setInterval(nextSlide, 3000)
+      currentPosition += 2
+      if (images.length == 2) currentPosition = 0
+      timer = setInterval(nextSlide, 3000)
+  } else {
+    document.getElementById('slideshow').innerHTML = `
+  <div class="slide" style="background-image: url('${images[0]}');"></div>
+  <div class="slide"></div>
+  `
+  }
 
   function nextSlide() {
     document.getElementById('slideshow').insertAdjacentHTML(
